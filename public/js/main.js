f34b5876308ca92452b44b12fff653903cee8529
Sherif <sherif.a.sarhan@gmail.com>
2017-06-12 03:40:29 -0400

    fixed hamburger(mobile) menu resizing on nav resize. now is centered properly

--- public/js/main.js
+++ public/js/main.js
@@ -4,79 +4,6 @@
  * ------------------------------------------------------------------- */ 
 
 (function($) {
-	// $(document).ready(function() {
-	// 	$('#contact_form').bootstrapValidator({
-	// 	    // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
-	// 	    feedbackIcons: {
-	// 	        valid: 'glyphicon glyphicon-ok',
-	// 	        invalid: 'glyphicon glyphicon-remove',
-	// 	        validating: 'glyphicon glyphicon-refresh'
-	// 	    },
-	// 	    fields: {
-	// 	        first_name: {
-	// 	            validators: {
-	// 	                    stringLength: {
-	// 	                    min: 2,
-	// 	                },
-	// 	                    notEmpty: {
-	// 	                    message: 'Please supply your first name'
-	// 	                }
-	// 	            }
-	// 	        },
-	// 	        last_name: {
-	// 	            validators: {
-	// 	                 stringLength: {
-	// 	                    min: 2,
-	// 	                },
-	// 	                notEmpty: {
-	// 	                    message: 'Please supply your last name'
-	// 	                }
-	// 	            }
-	// 	        },
-	// 	        email: {
-	// 	            validators: {
-	// 	                notEmpty: {
-	// 	                    message: 'Please supply your email address'
-	// 	                },
-	// 	                emailAddress: {
-	// 	                    message: 'Please supply a valid email address'
-	// 	                }
-	// 	            }
-	// 	        },
-	// 	        comment: {
-	// 	            validators: {
-	// 	                  stringLength: {
-	// 	                    min: 10,
-	// 	                    max: 200,
-	// 	                    message:'Please enter at least 10 characters and no more than 200'
-	// 	                },
-	// 	                notEmpty: {
-	// 	                    message: 'Please supply a description of your project'
-	// 	                }
-	// 	                }
-	// 	            }
-	// 	        }
-	// 	    })
-	// 	    .on('success.form.bv', function(e) {
-	// 	        $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
-	// 	            $('#contact_form').data('bootstrapValidator').resetForm();
-
-	// 	        // Prevent form submission
-	// 	        e.preventDefault();
-
-	// 	        // Get the form instance
-	// 	        var $form = $(e.target);
-
-	// 	        // Get the BootstrapValidator instance
-	// 	        var bv = $form.data('bootstrapValidator');
-
-	// 	        // Use Ajax to submit form data
-	// 	        $.post($form.attr('action'), $form.serialize(), function(result) {
-	// 	            console.log(result);
-	// 	        }, 'json');
-	// 	    });
-	// 	});
-
 	// initialize parallax. 
 	$('[data-parallax="scroll"]').each(function() {
     var $this = $(this);
@@ -90,9 +17,11 @@
 		if($(document).scrollTop()>400) {
 			$("#header").removeClass("large-nav").addClass("small-nav");
 			$("#header-nav-wrap").removeClass("large-wrap").addClass("small-wrap");
+			$(".header-menu-toggle").removeClass("large-toggle").addClass("small-toggle");
 		} else {
 			$("#header").removeClass("small-nav").addClass("large-nav");
 			$("#header-nav-wrap").removeClass("small-wrap").addClass("large-wrap");
+			$(".header-menu-toggle").removeClass("small-toggle").addClass("large-toggle");
 		}
 	
 	});
